d4d34ff847d4664658b48a50dd7b639c2e2a42ed
Yassine Birig <yassine@birig.me>
2025-11-21 19:32:15 +0100

    add cloudfalre

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [svelte()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  adapter: cloudflare()
 });
